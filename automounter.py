8e2dbbd8a2895dbaed2a922e28291ab9e4a3905f
arivarton <accounts@arivarton.com>
2015-12-02 22:20:26 +0100

    Removed unnecessary variables.

--- automounter.py
+++ automounter.py
@@ -42,13 +42,11 @@ for i in range(len(sys.argv)):
     i += 1
 
 count = 0
-timeAdded = 0
 
 while True:
     try:
         # Check if url that was set is reachable.
         urllib.request.urlopen(url)
-        timeAdded = 0
         # If the connection has been false or not checked before. Mount drives.
         if len(listdir(mountTo)) == 0:
             print(str(count) + " - " + time.strftime("%d.%m.%Y - %H:%M:%S: ") + "Opened " + url + " successfully. Mounting drives.")
